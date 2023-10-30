fdde5ebb3837fcb608979d49b2f98429b8fffe6e
kalil <kalilgb152@gmail.com>
2023-10-30 15:07:18 -0300

    deleted unnecessary folder

--- models/user.models.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/AngularJSdb');
-
-const UserSchema = new mongoose.Schema({
-  name: String,
-});
-
-const User = mongoose.model('User', UserSchema);
